b98671056baf1c0b61c8610afb8bd9e8cb9f0745
Rqa rqa <111213@unsaac.edu.pe>
2026-06-05 19:09:35 -0300

    Uso SDPT3 como solver en YALMIP

--- solve_selection_yalmip.m
+++ solve_selection_yalmip.m
@@ -5,9 +5,21 @@ function [delta_value, objective_value] = solve_selection_yalmip(M_prime_full, N
 %   s.t. delta(1:2Nr)=1, 0<=delta<=1, sum(delta)=2Nr+alpha.
 
 delta = sdpvar(M_prime_full, 1);
-objective_matrix = eye(2 * Nt) + 1 / lambda * (sigma_x^2 / 2) * (H_eff_r_q_full' * diag(delta) * H_eff_r_q_full);
-constraints = [delta(1:2*Nr) == 1, 0 <= delta <= 1, sum(delta) == 2 * Nr + alpha, objective_matrix >= 1e-9 * eye(2 * Nt)];
-diagnostics = optimize(constraints, -0.5 * logdet(objective_matrix), sdpsettings('verbose', 0));
+
+objective_matrix = eye(2 * Nt) + ...
+    (1 / lambda) * (sigma_x^2 / 2) * ...
+    (H_eff_r_q_full' * diag(delta) * H_eff_r_q_full);
+
+constraints = [
+    delta(1:2*Nr) == 1
+    0 <= delta <= 1
+    sum(delta) == 2 * Nr + alpha
+    objective_matrix >= 1e-9 * eye(2 * Nt)
+];
+
+options = sdpsettings('solver', 'sdpt3', 'verbose', 0);
+
+diagnostics = optimize(constraints, -0.5 * logdet(objective_matrix), options);
 
 if diagnostics.problem ~= 0
     error('YALMIP optimization failed. Code=%d, Info=%s', diagnostics.problem, diagnostics.info);
@@ -15,4 +27,5 @@ end
 
 delta_value = value(delta);
 objective_value = 0.5 * log(det(value(objective_matrix)));
+
 end
